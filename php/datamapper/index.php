fcf47a6c57fedc98537ddc46fd595bc5f06abf9d
CJ <weicongju@gmail.com>
2017-02-12 23:20:18 +0800

    data mapper create table

--- php/datamapper/index.php
+++ php/datamapper/index.php
@@ -1,12 +1,22 @@
 <?php
 
+$db = new PDO('sqlite:./database.sqlite3');
+
+$create_people_statement = <<<EOF
+CREATE TABLE people IF NOT EXISTS(
+    id INT AUTOINCREMENT PRIMARY KEY NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    number_of_dependents INT NOT NULL
+);
+EOF;
+$db->exec($create_people_statement);
+
 class Person
 {
     private $lastName;
     private $firstName;
     private $numberOfDependents;
-
-
 }
 
 abstract class AbstractMapper
